9754bebbf6fa24ba02ffdd9c3664cc2ef8f5a6c1
Angel D. Munoz <scythes.lol@gmail.com>
2020-08-17 16:46:16 -0600

    first commit

---
+++ Game1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended.ViewportAdapters;
+
+namespace Repro
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private TiledMapRenderer _tiledMapRenderer;
+        private TiledMap _map;
+        private OrthographicCamera _camera;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            var vp = GraphicsDevice.Viewport;
+            var vpAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, vp.Width, vp.Height);
+            _camera = new OrthographicCamera(vpAdapter);
+            _map = Content.Load<TiledMap>("Plazas/sample");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _map);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            _tiledMapRenderer.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin(transformMatrix: _camera.GetViewMatrix(), samplerState: SamplerState.PointClamp);
+            _tiledMapRenderer.Draw(_camera.GetViewMatrix());
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# MonoGame.Extended.Tiled Repro
+
+
+install paket as a global tool 
+
+- `dotnet tool install -g paket`
+- `paket install`
+- <kbd>F5</kbd> in vscode or VS2019
